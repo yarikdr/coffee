546c45b3261669c5b3da8a824a22593f57d704ba
Yaroslav <yarikdrapey@gmail.com>
2023-06-28 17:28:07 +0300

    btn go to top works

--- js/script.js
+++ js/script.js
@@ -10,6 +10,20 @@ function burger(burgerSel, menuSel) {
   })
 }
 
+function scrollToTop(btnSelector) {
+  const btn = document.querySelector(btnSelector)
+  btn.addEventListener('click', scroll)
+
+  function scroll() {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    })
+  }
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   burger('.burger', '.menu')
+  scrollToTop('.footer__btn')
 })
